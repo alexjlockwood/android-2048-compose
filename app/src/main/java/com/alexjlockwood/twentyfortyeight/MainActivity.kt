21ddaec292b220ff7d82ebf94cb30b966aeadf81
Yuya <140081005+oikvpqya@users.noreply.github.com>
2024-06-24 19:23:51 +0900

    refactor: Provide GameViewModel in Compose

--- app/src/main/java/com/alexjlockwood/twentyfortyeight/MainActivity.kt
+++ app/src/main/java/com/alexjlockwood/twentyfortyeight/MainActivity.kt
@@ -3,21 +3,20 @@ package com.alexjlockwood.twentyfortyeight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.alexjlockwood.twentyfortyeight.repository.GameRepository
 import com.alexjlockwood.twentyfortyeight.ui.AppTheme
 import com.alexjlockwood.twentyfortyeight.ui.GameUi
 import com.alexjlockwood.twentyfortyeight.viewmodel.GameViewModel
-import com.alexjlockwood.twentyfortyeight.viewmodel.GameViewModelFactory
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val gameViewModel by viewModels<GameViewModel> { GameViewModelFactory(this) }
-
         setContent {
+            val gameViewModel = viewModel { GameViewModel(GameRepository(application)) }
             AppTheme {
                 Surface {
                     GameUi(
